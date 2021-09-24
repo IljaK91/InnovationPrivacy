1b2bc6ca35b00eafa4662df9e052fb96e0714eda
Ilja Kantorovitch <iljakant@gmail.com>
2021-09-24 16:34:40 +0200

    typo

--- src/Functions.jl
+++ src/Functions.jl
@@ -2,7 +2,7 @@
     Realized firm profits taking the interest rate R as given
 """
 function firm_profit(a, K, par::Pars)
-    @unpack_Para par
+    @unpack_Pars par
     exp(a^((θ - 1)/θ))*K^α_tilde - R*K
 end
 
@@ -10,7 +10,7 @@ end
     Constructor for the Bayesian Parameters (weights on private signal and prior)
 """
 function BPar(β,par::Pars)
-    @unpack_Para par
+    @unpack_Pars par
     
     w_a = β/(β + σ²ₐ^-1)  
     w_p = σ²ₐ^-1/(β + σ²ₐ^-1)
