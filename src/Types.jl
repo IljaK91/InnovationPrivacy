ff0e4d1d64de8bc61671c439b980dbc9652ef7cc
Ilja Kantorovitch <iljakant@gmail.com>
2022-05-24 13:45:20 +0200

    Kick out second period code

--- src/Types.jl
+++ src/Types.jl
@@ -292,158 +292,158 @@ end
     cb_S = 2
     cb_U = 2
 end
-"""
-    The parameters and steady state values for the model written down by Ilja and Roxana.
-    Main features: Firms share structured which still needs to be processed internally to generate knowledge.
-    Additionally, data is partly non-rival (ν > 0). The rival-formulation is nested
+# """
+#     The parameters and steady state values for the model written down by Ilja and Roxana.
+#     Main features: Firms share structured which still needs to be processed internally to generate knowledge.
+#     Additionally, data is partly non-rival (ν > 0). The rival-formulation is nested
 
-"""
+# """
+# @with_kw struct Pars_v3
+#     #! Deep Parameters
+#     σ = 2.0  # Elasticity of Substitution
+#     c = ((σ - 1) / σ) # auxilary variable
+#     w_S = 0.5  # Share of Sophisticated Firms
+#     w_B = 0.5  # Share of Firms with large customer base.
+#     α = 0.3  # labor share, knowledge share is 1-α
+#     ϕ = 0.3  # diminishing returns data generation
+#     τ = 0.1  # icerberg transportation cost
+#     τ2 = 0.1  # icerberg transportation cost
+#     ν = 0.05 # non-rivalry parameter
+#     ε = 2    # elasticity of substition data bundle
+#     ξ = 1.0  # relative usefulness external data
+#     β = 0.94 # Discount Factor
+#     ζ = 1.0  #! Does not work and needs to be equal to 1 at the moment!!!!
+
+#     compl_prod::Symbol = :yes
+
+#     α_Y = set_a_Y(compl_prod, σ, ζ) # auxiliary paramter
+
+
+#     α_L_hat = (σ - 1) / σ * (1 - α) # adjusted labor factor share 
+#     α_K_hat = (σ - 1) / σ * α # adjusted knowledge factor share
+
+#     bundle = :yes # Switch in case I want to also implement a version without the data bundle, but making ε very large is similar
+
+#     λ = 0.1234
+
+#     #! Persistent Knowledge and Data
+#     δ_D = 0.1 # depreciation rate for data
+#     δ_K = 0.1 # depreciation rate for knowledge
+#     second_period = :yes
+
+#     #! Steady State Parameters
+#     P = 1  # aggregate price level
+#     #! This is the solution for the model with tau = 0, nu = 0, α_Y = 0
+#     Y = 2.075300672340325  # aggregate output
+#     w = 0.2367874818108683  # wage rate good production
+#     w_P = 0.04669426512765732  # wage rate data processing
+#     w_G = 0.06021347283561476  # wage rate data generation
+#     L = 1  # Labor supply for production of the intermediate good
+#     L_P = 1  # Labor supply for processing data
+#     L_G = 1  # Labor supply for generating / structuring data
+#     p_D = 0.11356059692613776  # Price of Structured Data
+#     Ω = 0.0 # data multiplier, generated minus iceberg transportation plus non-rival
+#     Ω_bundle = 0.0 # data multiplier, bundle relative to generated
+
+#     Y2 = 2.075300672340325  # aggregate output
+#     w2 = 0.2367874818108683  # wage rate good production
+#     w_P2 = 0.04669426512765732  # wage rate data processing
+#     w_G2 = 0.06021347283561476  # wage rate data generation
+#     L2 = 1  # Labor supply for production of the intermediate good
+#     L_P2 = 1  # Labor supply for processing data
+#     L_G2 = 1  # Labor supply for generating / structuring data
+#     p_D2 = 0.11356059692613776  # Price of Structured Data
+#     Ω2 = 0.0 # data multiplier, generated minus iceberg transportation plus non-rival
+#     Ω_bundle2 = 0.0 # data multiplier, bundle relative to generated
+
+#     D_G_SS = 0.0 # Total data generated
+#     D_S_SS = 0.0 # Total data shared
+
+#     D_G_SS2 = 0.0 # Total data generated
+#     D_S_SS2 = 0.0 # Total data shared
+
+#     #! Firm Specific Parameters
+#     A_G_common = 1.0
+#     A_G_unsoph = 0.2
+#     A_P_common = 1.0
+#     A_P_unsoph = 1.0
+
+#     A_G_common2 = 1.0
+#     A_P_common2 = 1.0
+
+#     A_G_S = A_G_unsoph * A_G_common # data generation productivity parameter, small
+#     A_G_B = 1.0 * A_G_common # data generation productivity parameter, big
+#     A_P_U = A_P_unsoph * A_P_common # data processing productivity parameter, unsophisticated
+#     A_P_S = 1.0 * A_P_common # data processing productivity parameter, sophisticated
+
+#     γ_S = 0.2  # labor share data processing sophisticated, data share is 1-γ
+#     γ_U = 0.4  # labor share data processing unsophisticated, data share is 1-γ
+
+#     Firm_sol = 0
+
+#     #! Solution Firm-Parameters
+#     l_LU_ss = 0 # Labor good production small customer base, unsophisticated
+#     l_BU_ss = 0 # Labor good production big customer base, unsophisticated
+#     l_LS_ss = 0 # Labor good production small customer base, sophisticated
+#     l_BS_ss = 0 # Labor good production big customer base, sophisticated
+
+#     l_P_LU_ss = 0 # Labor good production small customer base, unsophisticated
+#     l_P_BU_ss = 0 # Labor good production big customer base, unsophisticated
+#     l_P_LS_ss = 0 # Labor good production small customer base, sophisticated
+#     l_P_BS_ss = 0 # Labor good production big customer base, sophisticated
+
+#     l_G_LU_ss = 0 # Labor good production small customer base, unsophisticated
+#     l_G_BU_ss = 0 # Labor good production big customer base, unsophisticated
+#     l_G_LS_ss = 0 # Labor good production small customer base, sophisticated
+#     l_G_BS_ss = 0 # Labor good production big customer base, sophisticated
+
+#     D_S_LU_ss = 0 # data sharing small customer base, unsophisticated
+#     D_S_BU_ss = 0 # data sharing big customer base, unsophisticated
+#     D_S_LS_ss = 0 # data sharing small customer base, sophisticated
+#     D_S_BS_ss = 0 # data sharing big customer base, sophisticated
+
+#     D_G_LU_ss = 0 # data generated small customer base, unsophisticated
+#     D_G_BU_ss = 0 # data generated big customer base, unsophisticated
+#     D_G_LS_ss = 0 # data generated small customer base, sophisticated
+#     D_G_BS_ss = 0 # data generated big customer base, sophisticated
+
+#     D_I_LU_ss = 0 # data generated small customer base, unsophisticated
+#     D_I_BU_ss = 0 # data generated big customer base, unsophisticated
+#     D_I_LS_ss = 0 # data generated small customer base, sophisticated
+#     D_I_BS_ss = 0 # data generated big customer base, sophisticated
+
+#     D_E_LU_ss = 0 # data generated small customer base, unsophisticated
+#     D_E_BU_ss = 0 # data generated big customer base, unsophisticated
+#     D_E_LS_ss = 0 # data generated small customer base, sophisticated
+#     D_E_BS_ss = 0 # data generated big customer base, sophisticated
+
+#     D_LU_ss = 0 # data generated small customer base, unsophisticated
+#     D_BU_ss = 0 # data generated big customer base, unsophisticated
+#     D_LS_ss = 0 # data generated small customer base, sophisticated
+#     D_BS_ss = 0 # data generated big customer base, sophisticated
+
+#     D_last_LU = 0 # data generated small customer base, unsophisticated
+#     D_last_BU = 0 # data generated big customer base, unsophisticated
+#     D_last_LS = 0 # data generated small customer base, sophisticated
+#     D_last_BS = 0 # data generated big customer base, sophisticated
+
+#     K_LU_ss = 0 # knowledge employed small customer base, unsophisticated
+#     K_BU_ss = 0 # knowledge employed big customer base, unsophisticated
+#     K_LS_ss = 0 # knowledge employed small customer base, sophisticated
+#     K_BS_ss = 0 # knowledge employed big customer base, sophisticated
+
+#     K_last_LU = 0 # knowledge employed small customer base, unsophisticated
+#     K_last_BU = 0 # knowledge employed big customer base, unsophisticated
+#     K_last_LS = 0 # knowledge employed small customer base, sophisticated
+#     K_last_BS = 0 # knowledge employed big customer base, sophisticated
+
+#     prof_LU_ss = 0 # knowledge employed small customer base, unsophisticated
+#     prof_BU_ss = 0 # knowledge employed big customer base, unsophisticated
+#     prof_LS_ss = 0 # knowledge employed small customer base, sophisticated
+#     prof_BS_ss = 0 # knowledge employed big customer base, sophisticated
+# end
+
 @with_kw struct Pars_v3
-    #! Deep Parameters
-    σ = 2.0  # Elasticity of Substitution
-    c = ((σ - 1) / σ) # auxilary variable
-    w_S = 0.5  # Share of Sophisticated Firms
-    w_B = 0.5  # Share of Firms with large customer base.
-    α = 0.3  # labor share, knowledge share is 1-α
-    ϕ = 0.3  # diminishing returns data generation
-    τ = 0.1  # icerberg transportation cost
-    τ2 = 0.1  # icerberg transportation cost
-    ν = 0.05 # non-rivalry parameter
-    ε = 2    # elasticity of substition data bundle
-    ξ = 1.0  # relative usefulness external data
-    β = 0.94 # Discount Factor
-    ζ = 1.0  #! Does not work and needs to be equal to 1 at the moment!!!!
-
-    compl_prod::Symbol = :yes
-
-    α_Y = set_a_Y(compl_prod, σ, ζ) # auxiliary paramter
-
-
-    α_L_hat = (σ - 1) / σ * (1 - α) # adjusted labor factor share 
-    α_K_hat = (σ - 1) / σ * α # adjusted knowledge factor share
-
-    bundle = :yes # Switch in case I want to also implement a version without the data bundle, but making ε very large is similar
-
-    λ = 0.1234
-
-    #! Persistent Knowledge and Data
-    δ_D = 0.1 # depreciation rate for data
-    δ_K = 0.1 # depreciation rate for knowledge
-    second_period = :yes
-
-    #! Steady State Parameters
-    P = 1  # aggregate price level
-    #! This is the solution for the model with tau = 0, nu = 0, α_Y = 0
-    Y = 2.075300672340325  # aggregate output
-    w = 0.2367874818108683  # wage rate good production
-    w_P = 0.04669426512765732  # wage rate data processing
-    w_G = 0.06021347283561476  # wage rate data generation
-    L = 1  # Labor supply for production of the intermediate good
-    L_P = 1  # Labor supply for processing data
-    L_G = 1  # Labor supply for generating / structuring data
-    p_D = 0.11356059692613776  # Price of Structured Data
-    Ω = 0.0 # data multiplier, generated minus iceberg transportation plus non-rival
-    Ω_bundle = 0.0 # data multiplier, bundle relative to generated
-
-    Y2 = 2.075300672340325  # aggregate output
-    w2 = 0.2367874818108683  # wage rate good production
-    w_P2 = 0.04669426512765732  # wage rate data processing
-    w_G2 = 0.06021347283561476  # wage rate data generation
-    L2 = 1  # Labor supply for production of the intermediate good
-    L_P2 = 1  # Labor supply for processing data
-    L_G2 = 1  # Labor supply for generating / structuring data
-    p_D2 = 0.11356059692613776  # Price of Structured Data
-    Ω2 = 0.0 # data multiplier, generated minus iceberg transportation plus non-rival
-    Ω_bundle2 = 0.0 # data multiplier, bundle relative to generated
-
-    D_G_SS = 0.0 # Total data generated
-    D_S_SS = 0.0 # Total data shared
-
-    D_G_SS2 = 0.0 # Total data generated
-    D_S_SS2 = 0.0 # Total data shared
-
-    #! Firm Specific Parameters
-    A_G_common = 1.0
-    A_G_unsoph = 0.2
-    A_P_common = 1.0
-    A_P_unsoph = 1.0
-
-    A_G_common2 = 1.0
-    A_P_common2 = 1.0
-
-    A_G_S = A_G_unsoph * A_G_common # data generation productivity parameter, small
-    A_G_B = 1.0 * A_G_common # data generation productivity parameter, big
-    A_P_U = A_P_unsoph * A_P_common # data processing productivity parameter, unsophisticated
-    A_P_S = 1.0 * A_P_common # data processing productivity parameter, sophisticated
-
-    γ_S = 0.2  # labor share data processing sophisticated, data share is 1-γ
-    γ_U = 0.4  # labor share data processing unsophisticated, data share is 1-γ
-
-    Firm_sol = 0
-
-    #! Solution Firm-Parameters
-    l_LU_ss = 0 # Labor good production small customer base, unsophisticated
-    l_BU_ss = 0 # Labor good production big customer base, unsophisticated
-    l_LS_ss = 0 # Labor good production small customer base, sophisticated
-    l_BS_ss = 0 # Labor good production big customer base, sophisticated
-
-    l_P_LU_ss = 0 # Labor good production small customer base, unsophisticated
-    l_P_BU_ss = 0 # Labor good production big customer base, unsophisticated
-    l_P_LS_ss = 0 # Labor good production small customer base, sophisticated
-    l_P_BS_ss = 0 # Labor good production big customer base, sophisticated
-
-    l_G_LU_ss = 0 # Labor good production small customer base, unsophisticated
-    l_G_BU_ss = 0 # Labor good production big customer base, unsophisticated
-    l_G_LS_ss = 0 # Labor good production small customer base, sophisticated
-    l_G_BS_ss = 0 # Labor good production big customer base, sophisticated
-
-    D_S_LU_ss = 0 # data sharing small customer base, unsophisticated
-    D_S_BU_ss = 0 # data sharing big customer base, unsophisticated
-    D_S_LS_ss = 0 # data sharing small customer base, sophisticated
-    D_S_BS_ss = 0 # data sharing big customer base, sophisticated
-
-    D_G_LU_ss = 0 # data generated small customer base, unsophisticated
-    D_G_BU_ss = 0 # data generated big customer base, unsophisticated
-    D_G_LS_ss = 0 # data generated small customer base, sophisticated
-    D_G_BS_ss = 0 # data generated big customer base, sophisticated
-
-    D_I_LU_ss = 0 # data generated small customer base, unsophisticated
-    D_I_BU_ss = 0 # data generated big customer base, unsophisticated
-    D_I_LS_ss = 0 # data generated small customer base, sophisticated
-    D_I_BS_ss = 0 # data generated big customer base, sophisticated
-
-    D_E_LU_ss = 0 # data generated small customer base, unsophisticated
-    D_E_BU_ss = 0 # data generated big customer base, unsophisticated
-    D_E_LS_ss = 0 # data generated small customer base, sophisticated
-    D_E_BS_ss = 0 # data generated big customer base, sophisticated
-
-    D_LU_ss = 0 # data generated small customer base, unsophisticated
-    D_BU_ss = 0 # data generated big customer base, unsophisticated
-    D_LS_ss = 0 # data generated small customer base, sophisticated
-    D_BS_ss = 0 # data generated big customer base, sophisticated
-
-    D_last_LU = 0 # data generated small customer base, unsophisticated
-    D_last_BU = 0 # data generated big customer base, unsophisticated
-    D_last_LS = 0 # data generated small customer base, sophisticated
-    D_last_BS = 0 # data generated big customer base, sophisticated
-
-    K_LU_ss = 0 # knowledge employed small customer base, unsophisticated
-    K_BU_ss = 0 # knowledge employed big customer base, unsophisticated
-    K_LS_ss = 0 # knowledge employed small customer base, sophisticated
-    K_BS_ss = 0 # knowledge employed big customer base, sophisticated
-
-    K_last_LU = 0 # knowledge employed small customer base, unsophisticated
-    K_last_BU = 0 # knowledge employed big customer base, unsophisticated
-    K_last_LS = 0 # knowledge employed small customer base, sophisticated
-    K_last_BS = 0 # knowledge employed big customer base, sophisticated
-
-    prof_LU_ss = 0 # knowledge employed small customer base, unsophisticated
-    prof_BU_ss = 0 # knowledge employed big customer base, unsophisticated
-    prof_LS_ss = 0 # knowledge employed small customer base, sophisticated
-    prof_BS_ss = 0 # knowledge employed big customer base, sophisticated
-end
-
-@with_kw struct Pars_v3_simpl
     #! Deep Parameters
     σ = 2.0  # Elasticity of Substitution
     c = (σ - 1) / σ # auxilary variable
